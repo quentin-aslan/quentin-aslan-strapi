45ddccc100268125250e700c2ef4ca719f9af4ec
Aslan <quentin.aslan@outlook.com>
2025-04-07 23:09:30 -0400

    :sparkles: CI/CD implemented, sqlite database added to the repositoru. package.json and package-lock cleaned to allow npm ci.

--- config/database.ts
+++ config/database.ts
@@ -44,7 +44,7 @@ export default ({ env }) => {
     },
     sqlite: {
       connection: {
-        filename: path.join(__dirname, '..', '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+        filename: path.join(__dirname, '..', '..', env('DATABASE_FILENAME', '.tmp/data.db')), // updated via .env to ./database_sqlite/data.db
       },
       useNullAsDefault: true,
     },
